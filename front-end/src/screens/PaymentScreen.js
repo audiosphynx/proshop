39d0aec95ebd2b2bc336a48d37aa7179b1e76094
Mark Berman <7337206+audiosphynx@users.noreply.github.com>
2021-04-01 15:21:17 +0200

    Payment Screen & Save Payment Method

---
+++ front-end/src/screens/PaymentScreen.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+import { Form, Button, Col } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import FormContainer from "../components/FormContainer";
+import CheckoutSteps from "../components/CheckoutSteps";
+// import { savePaymentMehtod } from "../actions/cartActions";
+
+function PaymentScreen({ history }) {
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
+
+  const dispatch = useDispatch();
+  // Add paypal as default method. Lateron you can make it an empty string and let the user select an option
+  const [paymentMethod, setPaymentMethod] = useState("Paypal");
+
+  if (!shippingAddress.address) {
+    history.push("/shipping");
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    //  dispatch(savePaymentMethod(paymentMethod));
+    history.push("/placeorder");
+  };
+
+  return (
+    <FormContainer>
+      <CheckoutSteps step1 step2 step3 />
+
+      <Form onSubmit={submitHandler}>
+        <Form.Group>
+          <Form.Label as="legend">Select Method</Form.Label>{" "}
+          <Col>
+            <Form.Check
+              type="radio"
+              // Thi is where you can add in custom methods like stripe
+              label="Paypal or Credit Card"
+              id="Paypal"
+              name="paymentMethod"
+              checked
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            ></Form.Check>
+          </Col>
+        </Form.Group>
+
+        <Button type="submit" variant="primary">
+          Continue
+        </Button>
+      </Form>
+    </FormContainer>
+  );
+}
+
+export default PaymentScreen;
