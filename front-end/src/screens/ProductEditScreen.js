b5ec2e0ec951fdd1a6a87cf67f4d388b0d88f9a4
Mark Berman <7337206+audiosphynx@users.noreply.github.com>
2021-04-03 15:56:19 +0200

    Edit Product Functionality

--- front-end/src/screens/ProductEditScreen.js
+++ front-end/src/screens/ProductEditScreen.js
@@ -6,10 +6,8 @@ import { useDispatch, useSelector } from "react-redux";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
 import FormContainer from "../components/FormContainer";
-import {
-  listProductDetails /*updateProduct*/,
-} from "../actions/productActions";
-//import { PRODUCT_UPDATE_RESET } from "../constants/productConstants";
+import { listProductDetails, updateProduct } from "../actions/productActions";
+import { PRODUCT_UPDATE_RESET } from "../constants/productConstants";
 
 function ProductEditScreen({ match, history }) {
   const productId = match.params.id;
@@ -28,35 +26,35 @@ function ProductEditScreen({ match, history }) {
   const productDetails = useSelector((state) => state.productDetails);
   const { error, loading, product } = productDetails;
 
-  /*const productUpdate = useSelector((state) => state.productUpdate);
+  const productUpdate = useSelector((state) => state.productUpdate);
   const {
     error: errorUpdate,
     loading: loadingUpdate,
     success: successUpdate,
   } = productUpdate;
-	*/
 
   useEffect(() => {
-    /* if (successUpdate) {
+    if (successUpdate) {
       dispatch({ type: PRODUCT_UPDATE_RESET });
       history.push("/admin/productlist");
-    } else { */
-    if (!product.name || product._id !== Number(productId)) {
-      dispatch(listProductDetails(productId));
     } else {
-      setName(product.name);
-      setPrice(product.price);
-      setImage(product.image);
-      setBrand(product.brand);
-      setCategory(product.category);
-      setCountInStock(product.countInStock);
-      setDescription(product.description);
+      if (!product.name || product._id !== Number(productId)) {
+        dispatch(listProductDetails(productId));
+      } else {
+        setName(product.name);
+        setPrice(product.price);
+        setImage(product.image);
+        setBrand(product.brand);
+        setCategory(product.category);
+        setCountInStock(product.countInStock);
+        setDescription(product.description);
+      }
     }
-  }, [dispatch, product, productId, history /*successUpdate*/]);
+  }, [dispatch, product, productId, history, successUpdate]);
 
   const submitHandler = (e) => {
     e.preventDefault();
-    /*  dispatch(
+    dispatch(
       updateProduct({
         _id: productId,
         name,
@@ -67,7 +65,7 @@ function ProductEditScreen({ match, history }) {
         countInStock,
         description,
       })
-    ); */
+    );
   };
 
   /* const uploadFileHandler = async (e) => {
@@ -105,8 +103,8 @@ function ProductEditScreen({ match, history }) {
 
       <FormContainer>
         <h1>Edit Product</h1>
-        {/*}  {loadingUpdate && <Loader />}
-        {errorUpdate && <Message variant="danger">{errorUpdate}</Message>} */}
+        {loadingUpdate && <Loader />}
+        {errorUpdate && <Message variant="danger">{errorUpdate}</Message>}
 
         {loading ? (
           <Loader />
