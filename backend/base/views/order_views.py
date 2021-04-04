f5cecd0b57faa2698689906db5976b92b50badb8
Mark Berman <7337206+audiosphynx@users.noreply.github.com>
2021-04-04 10:29:44 +0200

    Admin Order List

--- backend/base/views/order_views.py
+++ backend/base/views/order_views.py
@@ -67,6 +67,14 @@ def getMyOrders(request):
 @api_view(['GET'])
 @permission_classes([IsAdminUser])
 def getOrders(request):
+    orders = Orders.objects.all()
+    serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getOrders(request):
     orders = Order.objects.all()
     serializer = OrderSerializer(orders, many=True)
     return Response(serializer.data)
